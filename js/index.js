ad42e477568112e7fe302baa0f8f2e2bc1c9f9bf
uc-sks <shivam.singh@ucertify.com>
2022-02-15 14:55:06 +0530

    fixes bugs

--- js/index.js
+++ js/index.js
@@ -29,149 +29,135 @@ let randomArr = [];
 // getting and stop the time
 let downloadTimer;
 //fliped card id
-let allCard=[]
+let allCard = []
 //generating random data
-function genRandNum(value){
+function genRandNum(value) {
     while (randomArr.length < value) {
         const randomData = Math.floor(Math.random() * value) + 0;
         if (randomArr.indexOf(randomData) === -1) randomArr.push(randomData);
-    } 
+    }
+}
+function option() {
+    document.getElementById("2_pic").disabled = true;
+    document.getElementById("3_pic").disabled = true;
+    document.getElementById("4_pic").disabled = true;
+    document.getElementById("show").disabled = false;
+    document.getElementById("newGame").disabled = false;
+}
+function eventNone() {
+    const card = document.querySelectorAll(".card");
+    card.forEach(function (card) {
+        card.classList.remove('eventNuno')
+    });
+}
+function setBackImage() {
+    for (i = 0; i < randomArr.length; i++) {
+        document.getElementById(`imgs${i}`).src = twoGameData[randomArr[i]]
+    }
 }
 // generating grid of 24 card here
-for(i=0;i<24;i++){
+for (i = 0; i < 24; i++) {
     const x = document.createElement("div");
-        x.setAttribute("class", "card eventNuno");
-        x.setAttribute("id", `card${i}`);
-        x.setAttribute("onclick", "flipCard(event)");
-        document.getElementById("sub_container__1").appendChild(x);
-        const y = document.createElement("div");
-        y.setAttribute("class", "front");
-        y.setAttribute("id", `front${i}`);
-        document.getElementById(`card${i}`).appendChild(y);
-        const y1 = document.createElement("img");
-        y1.setAttribute("class", "img1");
-        y1.setAttribute("id", `img${i}`);
-        y1.setAttribute("src", "./img/img2.jpeg");
-        y1.setAttribute("alt", "");
-        document.getElementById(`front${i}`).appendChild(y1);
-        const z = document.createElement("div");
-        z.setAttribute("class", "back");
-        z.setAttribute("id", `back${i}`);
-        document.getElementById(`card${i}`).appendChild(z);
-        const z1 = document.createElement("img");
-        z1.setAttribute("class", "img1");
-        z1.setAttribute("id", `imgs${i}`);
-        z1.setAttribute("src", "./img/img2.jpeg");
-        z1.setAttribute("alt", "");
-        document.getElementById(`back${i}`).appendChild(z1);
-        // num++
+    x.setAttribute("class", "card eventNuno");
+    x.setAttribute("id", `card${i}`);
+    x.setAttribute("onclick", "flipCard(event)");
+    document.getElementById("sub_container__1").appendChild(x);
+    const y = document.createElement("div");
+    y.setAttribute("class", "front");
+    y.setAttribute("id", `front${i}`);
+    document.getElementById(`card${i}`).appendChild(y);
+    const y1 = document.createElement("img");
+    y1.setAttribute("class", "img1");
+    y1.setAttribute("id", `img${i}`);
+    y1.setAttribute("src", "./img/img2.jpeg");
+    y1.setAttribute("alt", "");
+    document.getElementById(`front${i}`).appendChild(y1);
+    const z = document.createElement("div");
+    z.setAttribute("class", "back");
+    z.setAttribute("id", `back${i}`);
+    document.getElementById(`card${i}`).appendChild(z);
+    const z1 = document.createElement("img");
+    z1.setAttribute("class", "img1");
+    z1.setAttribute("id", `imgs${i}`);
+    z1.setAttribute("src", "./img/img2.jpeg");
+    z1.setAttribute("alt", "");
+    document.getElementById(`back${i}`).appendChild(z1);
+    // num++
 }
 //2 series game functionality
 function twoGame() {
-    const card = document.querySelectorAll(".card");
-    card.forEach(function (card) {
-        card.classList.remove('eventNuno')
-    });
+    eventNone()
+    option()
     genRandNum(12)
-    randomArr=[... randomArr,... randomArr]
+    randomArr = [...randomArr, ...randomArr]
     randomArr = randomArr.sort(() => Math.random() - 0.5)
-    // console.log('random index', randomArr)
-    document.getElementById("2_pic").disabled = true;
+    console.log('random index', randomArr)
     document.getElementById("2_pic").checked = true;
-    document.getElementById("3_pic").disabled = true;
-    document.getElementById("4_pic").disabled = true;
-    document.getElementById("show").disabled = false;
-    document.getElementById("newGame").disabled = false;
     countDown();
-    for (i = 0; i <= randomArr.length; i++) {
-        document.getElementById(`imgs${i}`).src = twoGameData[randomArr[i]]
-    }
+    setBackImage()
 }
 //3 series game functionality
 function threeGame() {
-    const card = document.querySelectorAll(".card");
-    card.forEach(function (card) {
-        card.classList.remove('eventNuno')
-    });
-    document.getElementById("2_pic").disabled = true;
-    document.getElementById("3_pic").disabled = true;
+    eventNone()
     document.getElementById("3_pic").checked = true;
-    document.getElementById("4_pic").disabled = true;
-    document.getElementById("show").disabled = false;
-    document.getElementById("newGame").disabled = false;
+    option()
     countDown();
     genRandNum(8)
-    randomArr=[... randomArr,... randomArr,...randomArr]
+    randomArr = [...randomArr, ...randomArr, ...randomArr]
     randomArr = randomArr.sort(() => Math.random() - 0.5)
-    // console.log('random index', randomArr)
-    for (i = 0; i <= randomArr.length; i++) {
-        document.getElementById(`imgs${i}`).src = twoGameData[randomArr[i]]
-    }
+    console.log('random index', randomArr)
+    setBackImage()
 }
 //4 series game functionality
 function fourGame() {
-    const card = document.querySelectorAll(".card");
-    card.forEach(function (card) {
-        card.classList.remove('eventNuno')
-    });
-    document.getElementById("2_pic").disabled = true;
-    document.getElementById("3_pic").disabled = true;
-    document.getElementById("4_pic").disabled = true;
+    eventNone()
     document.getElementById("4_pic").checked = true;
-    document.getElementById("show").disabled = false;
-    document.getElementById("newGame").disabled = false;
+    option()
     countDown();
     genRandNum(6)
-    randomArr=[... randomArr,... randomArr,...randomArr,...randomArr]
+    randomArr = [...randomArr, ...randomArr, ...randomArr, ...randomArr]
     randomArr = randomArr.sort(() => Math.random() - 0.5)
-    // console.log('random index', randomArr)
-    for (i = 0; i <= randomArr.length; i++) {
-        document.getElementById(`imgs${i}`).src = twoGameData[randomArr[i]]
-    }
+    console.log('random index', randomArr)
+    setBackImage()
 }
 //fliping functionality(visibility or flip return for two series)
-function flipImage() {
-    // console.log('haa bhai aa gya');
-    if (twoSeriesArr[0] === twoSeriesArr[1]) {
-        result = result + 1;
-        // console.log('result is ', result)
+function removeFlipClass() {
+    const card1 = document.getElementById(`${cardId[0]}`);
+    const card2 = document.getElementById(`${cardId[1]}`);
+    setTimeout(function () {
+        card1.classList.remove("flipCard");
+        card2.classList.remove("flipCard");
         twoSeriesArr = []
-        // console.log('card id is',cardId)
-        allCard=allCard.concat(cardId)
-        // console.log('all card is',allCard)
         cardId = []
+    }, 200)
+}
+function stayVisible() {
+    result = result + 1;
+    console.log('result is ', result)
+    twoSeriesArr = []
+    // console.log('card id is',cardId)
+    allCard = allCard.concat(cardId)
+    // console.log('all card is',allCard)
+    cardId = []
+}
+function flipImage() {
+    if (twoSeriesArr[0] === twoSeriesArr[1]) {
+        stayVisible()
     } else {
-        const card1 = document.getElementById(`${cardId[0]}`);
-        const card2 = document.getElementById(`${cardId[1]}`);
-        setTimeout(function () {
-            card1.classList.remove("flipCard");
-            card2.classList.remove("flipCard");
-            twoSeriesArr = []
-            cardId = []
-        }, 200)
+        removeFlipClass()
     }
 }
 //fliping functionality(visibility or flip return for three series......for two pic)
 function flipImageSeries3() {
     if (twoSeriesArr[0] === twoSeriesArr[1]) {
     } else {
-        const card1 = document.getElementById(`${cardId[0]}`);
-        const card2 = document.getElementById(`${cardId[1]}`);
-        setTimeout(function () {
-            card1.classList.remove("flipCard");
-            card2.classList.remove("flipCard");
-            twoSeriesArr = []
-            cardId = []
-        }, 200)
+        removeFlipClass()
     }
 }
 function flipImageSeries3_1() {
     threeSeriesArray = [...new Set(twoSeriesArr)]
     if (threeSeriesArray.length == 1) {
-        result = result + 1;
-        twoSeriesArr = []
-        allCard=allCard.concat(cardId)
-        cardId = []
+        stayVisible()
     } else {
         const card1 = document.getElementById(`${cardId[0]}`);
         const card2 = document.getElementById(`${cardId[1]}`);
@@ -188,14 +174,7 @@ function flipImageSeries3_1() {
 function flipImagesSeries4() {
     if (twoSeriesArr[0] === twoSeriesArr[1]) {
     } else {
-        const card1 = document.getElementById(`${cardId[0]}`);
-        const card2 = document.getElementById(`${cardId[1]}`);
-        setTimeout(function () {
-            card1.classList.remove("flipCard");
-            card2.classList.remove("flipCard");
-            twoSeriesArr = []
-            cardId = []
-        }, 200)
+        removeFlipClass()
     }
 }
 function flipImagesSeries4_1() {
@@ -217,10 +196,7 @@ function flipImagesSeries4_1() {
 function flipImagesSeries4_1_2() {
     fourSeriesArray = [...new Set(twoSeriesArr)]
     if (fourSeriesArray.length == 1) {
-        result = result + 1;
-        twoSeriesArr = []
-        allCard=allCard.concat(cardId)
-        cardId = []
+        stayVisible()
     } else {
         const card1 = document.getElementById(`${cardId[0]}`);
         const card2 = document.getElementById(`${cardId[1]}`);
@@ -245,7 +221,6 @@ function show() {
     card.forEach(function (card) {
         card.classList.add('eventNuno')
     });
-    // switchIntoGame()
     cardId = []
     clearInterval(downloadTimer);
     card.forEach(function (card) {
@@ -300,6 +275,13 @@ function countDown() {
 // getting the card id
 function flipCard(event) {
     cardId.push(event.currentTarget.id)
+    // console.log('card id is',cardId)
+    const toFindDuplicates = cardId => cardId.filter((item, index) => cardId.indexOf(item) !== index)
+    const data = toFindDuplicates(cardId)
+    if (data.length != 0) {
+        cardId.pop()
+        twoSeriesArr.pop()
+    }
 }
 document.addEventListener("DOMContentLoaded", function () {
     const allCards = document.getElementsByClassName('card');
@@ -313,35 +295,37 @@ document.addEventListener("DOMContentLoaded", function () {
     const allCards1 = document.getElementsByClassName('card');
     Array.prototype.forEach.call(allCards1, function (element1) {
         element1.addEventListener('click', function () {
-            if(element1.classList.contains("flipCard")){
-            const children1 = element1.children[1].children[0]
-            const flipData1 = children1.src
-            let isFounded = allCard.some( data => cardId.includes(data) );
-            if(!isFounded){
-                twoSeriesArr.push(flipData1)
-            }else{
-                cardId.pop()
+            if (element1.classList.contains("flipCard")) {
+                const children1 = element1.children[1].children[0]
+                const flipData1 = children1.src
+                console.log('all card', allCard)
+                let isFounded = allCard.some(data => cardId.includes(data));
+                if (!isFounded) {
+                    twoSeriesArr.push(flipData1)
+                    console.log('two series ', twoSeriesArr)
+                } else {
+                    cardId.pop()
+                }
+                if (document.getElementById('2_pic').checked) {
+                    if (twoSeriesArr.length == 2) {
+                        flipImage();
+                    }
+                } else if (document.getElementById('3_pic').checked) {
+                    if (twoSeriesArr.length == 2) {
+                        flipImageSeries3()
+                    } else if (twoSeriesArr.length == 3) {
+                        flipImageSeries3_1()
+                    }
+                } else {
+                    if (twoSeriesArr.length == 2) {
+                        flipImagesSeries4();
+                    } else if (twoSeriesArr.length == 3) {
+                        flipImagesSeries4_1();
+                    } else if (twoSeriesArr.length == 4) {
+                        flipImagesSeries4_1_2();
+                    }
+                }
             }
-            if (document.getElementById('2_pic').checked) {
-                if (twoSeriesArr.length == 2) {
-                    flipImage();
-                }
-            } else if (document.getElementById('3_pic').checked) {
-                if (twoSeriesArr.length == 2) {
-                    flipImageSeries3()
-                } else if (twoSeriesArr.length == 3) {
-                    flipImageSeries3_1()
-                }
-            } else {
-                if (twoSeriesArr.length == 2) {
-                    flipImagesSeries4();
-                } else if (twoSeriesArr.length == 3) {
-                    flipImagesSeries4_1();
-                } else if (twoSeriesArr.length == 4) {
-                    flipImagesSeries4_1_2();
-                }
-            }
-        }
         });
     });
 });
@@ -363,21 +347,11 @@ function newGame() {
     threeSeriesArray = []
     fourSeriesArray = []
     cardId = []
-    allCard=[]
-    lengthOfClass = 0
+    allCard = []
     result = 0
     randomArr = []
-    console.log('random array', randomArr)
-    console.log('new card id new', cardId)
-    console.log('new array id', twoSeriesArr)
     timeLeft = 60
     document.getElementById("coutDown").innerHTML = `${timeLeft}`;
     clearInterval(downloadTimer)
-    if (document.getElementById("2_pic").checked || document.getElementById("3_pic").checked || document.getElementById("4_pic").checked) {
-        countDown()
-    }
-    for (i = 0; i <= 23; i++) {
-        document.getElementById(`imgs${i}`).src = ''
-    }
 }
 
